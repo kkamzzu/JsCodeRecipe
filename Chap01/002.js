bb3017960a71f5a31064f43f87f67bcbe7237800
kkamzzu <kkamzzu@gmail.com>
2023-04-03 23:21:19 +0900

    End[002]

---
+++ Chap01/002.js
@@ -0,0 +1,24 @@
+/* _____________ 자바스크립트의 구현(사용) 방식 _____________ */
+
+/*
+  HTML에서 script 태그로 감싼 부분이 자바스크립트를 기술하는 곳이다.
+  alert()를 사용하려면 하단처럼 html에 script를 넣어주면 된다.
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    // here
+    <script>
+      alert('안녕하세요!');
+    </script>
+  </head>
+  <body>
+    
+  </body>
+  </html>
+*/
